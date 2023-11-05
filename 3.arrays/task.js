282fa453792af4d07f4d7847401080a5ebda6e54
RLVictor <vityad888@yandex.ru>
2023-11-05 22:09:07 +0300

    Задание Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])
+  );
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter((item) => item.gender === gender);
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+  const ageSum = filteredUsers.reduce((acc, item) => acc + item.age, 0);
+  const result = ageSum / filteredUsers.length;
+  return result;
 }
