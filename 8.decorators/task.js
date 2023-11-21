94db3c4108508d9f7009f2670fdc34b408e928c4
RLVictor <vityad888@yandex.ru>
2023-11-21 21:19:50 +0300

    Первое задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,24 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  const cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.result);
+      return "Из кеша: " + objectInCache.result;
+    }
+    const result = func(...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+function debounceDecoratorNew(func, delay) {}
